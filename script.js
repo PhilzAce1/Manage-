7ba69f22f53f17f5b0169bbff92e62cc1ecb9f5b
Philzace1 <unknownumeh@gmail.com>
2020-07-29 00:30:22 +0100

    done

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const navBtn = document.querySelector('#toggle');
 const navPane = document.querySelector('.nav_pane');
 
-navBtn.addEventListener('click', (e) => {
-  const state = e.target.attributes['data-toggle'].value;
-  if (state === 'is-open') {
-    closeNav();
-    e.target.attributes['data-toggle'].value = 'is-closed';
-  } else {
-    openNav();
-    e.target.attributes['data-toggle'].value = 'is-open';
-  }
-});
+navBtn.addEventListener(
+  'click',
+  (e) => {
+    const state = e.currentTarget.attributes['data-toggle'].value;
+    if (state === 'is-open') {
+      closeNav();
+      e.currentTarget.attributes['data-toggle'].value = 'is-closed';
+    } else {
+      openNav();
+      e.currentTarget.attributes['data-toggle'].value = 'is-open';
+    }
+  },
+  true
+);
 
 function openNav() {
   navPane.style.display = 'inline-block';
